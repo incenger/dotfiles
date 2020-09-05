721559e72bd0884cde2713b88b349863f74fbe7e
incenger <dangtam.letan@gmail.com>
2020-09-05 12:27:05 +0700

    Update rofi

--- rofi/rofi_google_search.sh
+++ rofi/rofi_google_search.sh
@@ -1,9 +1,8 @@
 URL='https://www.google.com/search?q='
+PROMPT="Google:"
 
-QUERY=$(echo "" | rofi -dmenu -i -p  "Google" -theme-str 'window {height: 5%;}'); 
+QUERY=$(echo "" | rofi -dmenu -i -p  ${PROMPT} -theme-str 'window {height: 5%;}'); 
 
 if [ -n "$QUERY" ]; then
   xdg-open "${URL}${QUERY}" 2> /dev/null
 fi
-
-
