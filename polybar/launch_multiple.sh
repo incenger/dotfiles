6a631a43cce16332186bd5e52dbbbdbf8e4fc644
incenger <dangtam.letan@gmail.com>
2021-01-11 10:01:43 +0700

    Update polybar scripts

--- polybar/launch_multiple.sh
+++ polybar/launch_multiple.sh
@@ -6,6 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar --reload mybar &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar &
+fi
 
 echo "Bars launched..."
