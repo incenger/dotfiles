c726c3a01cc21741619cc7ac159f57d2da5eb86f
Tam Le <dangtam.letan@gmail.com>
2021-09-05 17:41:04 +0700

    Change rofi theme for rofi 1.7

--- rofi/rofi_google_search.sh
+++
@@ -1,8 +0,0 @@
-URL='https://www.google.com/search?q='
-PROMPT="Google:"
-
-QUERY=$(echo "" | rofi -dmenu -i -p  ${PROMPT} -theme-str 'window {height: 5%;}'); 
-
-if [ -n "$QUERY" ]; then
-  xdg-open "${URL}${QUERY}" 2> /dev/null
-fi
