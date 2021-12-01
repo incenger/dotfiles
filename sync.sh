a9b391d3efb9f0f1d3d9251e921afc996e69a4bf
Tam Le <dangtam.letan@gmail.com>
2021-12-01 20:38:23 +0700

    Add dunst

--- sync.sh
+++ sync.sh
@@ -4,6 +4,7 @@ rsync -a ~/.i3/ ./i3/ --delete
 rsync -a ~/.Xresources  ./
 rsync -a ~/.config/polybar/ ./polybar --delete
 rsync -a ~/.config/rofi/ ./rofi --delete
+rsync -a ~/.config/dunst/ ./dunst --delete
 rsync -a ~/.config/starship.toml ./starship --delete
 rsync -a ~/.tmux.conf ~/.tmux.theme ~/tmuxinator.zsh  ./tmux
 rsync -a ~/.zshrc ./
