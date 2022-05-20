d2bb6556f4f0cbd908f274ddd20504f914faafa8
Tam Le <dangtam.letan@gmail.com>
2022-05-20 19:54:02 +0700

    Change screenlayout rofi theme and order

--- rofi/rofi_screen_layout.sh
+++ rofi/rofi_screen_layout.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [ -z "$@" ]; then
-    echo -en "Home\n"
+    echo -en "Extended (Right)\n"
+    echo -en "Extended (Left)\n"
     echo -en "Mirror\n"
     echo -en "eDP1\n"
 else
@@ -9,7 +10,9 @@ else
         sh ~/.screenlayout/edp1.sh &> /dev/null & disown;
     elif [ "$1" = "Mirror" ]; then
         sh ~/.screenlayout/mirror.sh &> /dev/null & disown;
-    elif [ "$1" = "Home" ]; then
-        sh ~/.screenlayout/home_layout.sh &> /dev/null & disown;
+    elif [ "$1" = "Extended (Right)" ]; then
+        sh ~/.screenlayout/home_layout_right.sh &> /dev/null & disown;
+    elif [ "$1" = "Extended (Left)" ]; then
+        sh ~/.screenlayout/home_layout_left.sh &> /dev/null & disown;
     fi
 fi
