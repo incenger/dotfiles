455f80b914fe672c031b0761330925b0e49900b1
Tam le <dangtam.letan@gmail.com>
2022-07-31 00:27:10 +0700

    Ignore source files if not exist in rysnc

--- sync.sh
+++ sync.sh
@@ -1,10 +1,23 @@
-rsync -a ~/.config/alacritty/ ./alacritty --delete
-rsync -a ~/.config/nvim/ ./nvim --delete
-rsync -a ~/.i3/ ./i3/ --delete
-rsync -a ~/.Xresources  ./
-rsync -a ~/.config/polybar/ ./polybar --delete
-rsync -a ~/.config/rofi/ ./rofi --delete
-rsync -a ~/.config/dunst/ ./dunst --delete
-rsync -a ~/.config/starship.toml ./starship --delete
-rsync -a ~/.tmux.conf ~/.tmux.theme ~/tmuxinator.zsh  ./tmux
-rsync -a ~/.zshrc ./
+#!/bin/bash
+
+sync_with_ignore() {
+    for source_file in "${@:1:$#-1}"
+    do
+        if [[ ! -f $source_file ]] && [[ ! -d $source_file ]]
+        then
+            return
+        fi
+    done
+    rsync -a "$@" --delete
+}
+
+sync_with_ignore ~/.config/alacritty/ ./alacritty
+sync_with_ignore ~/.config/nvim/ ./nvim
+sync_with_ignore ~/.i3/ ./i3/
+sync_with_ignore ~/.Xresources  ./
+sync_with_ignore ~/.config/polybar/ ./polybar
+sync_with_ignore ~/.config/rofi/ ./rofi
+sync_with_ignore ~/.config/dunst/ ./dunst
+sync_with_ignore ~/.config/starship.toml ./starship
+sync_with_ignore ~/.tmux.conf ~/.tmux.theme ~/tmuxinator.zsh  ./tmux
+sync_with_ignore ~/.zshrc ./
